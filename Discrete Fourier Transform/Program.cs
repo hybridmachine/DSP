077e51f34ca4ff6f87133395c25e4d8f90ca45ba
Brian Tabone <brian_tabone@hotmail.com>
2018-06-16 17:25:36 -0500

    Initial Checkin

---
+++ Discrete Fourier Transform/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalGenerator;
+using System.IO;
+
+namespace Discrete_Fourier_Transform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> signals = Sinusoid.GetSignal(32, 2);
+
+            FileStream output = new FileStream("sinusoid.csv", FileMode.Create);
+
+            if (output.CanWrite)
+            {
+                StreamWriter writer = new StreamWriter(output);
+                int idx = 0;
+                foreach (double signal in signals)
+                {
+                    writer.WriteLine(idx + "," + signal);
+                    idx++;
+                }
+                writer.Close();
+            }
+            output.Close();
+        }
+    }
+}
---
+++ SignalGenerator/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalGenerator
+{
+    public class Sinusoid
+    {
+        // Generate a simple sinusoid
+        public static List<double> GetSignal(int sampleCount, int cycles)
+        {
+            List<double> samples = new List<double>(sampleCount);
+            double theta;
+
+            for (int sample = 0; sample < sampleCount; sample++)
+            {
+                theta = cycles * ((double)sample / (double)(sampleCount - 1)) * 2 * Math.PI;
+                Console.Out.WriteLine("Sample " + sample + " radian " + theta);
+                samples.Add(Math.Sin(theta));
+            }
+            return samples;
+        }
+    }
+}
