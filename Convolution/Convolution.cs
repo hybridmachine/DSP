ff42a511f689669a9300e89c71fb1cee76690f15
Brian Tabone <brian_tabone@hotmail.com>
2018-11-15 20:43:15 -0600

    Added convolution input side equation system based on http://www.dspguide.com/ch6/3.htm

---
+++ Convolution/Convolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSP
+{
+    public class Convolution
+    {
+        // This algorithm is derived from Table 6-1 of ISBN 0-9660176-3-3 "The Scientist and Engineer's Guide to Digital Signal Processing"
+        // http://www.dspguide.com/ch6/3.htm
+        public List<double> Convolve(List<double> kernel, List<double> input)
+        {
+            List<double> output = new List<double>(kernel.Count + input.Count - 1);
+
+            // Zero the output
+            for (int idx = 0; idx < output.Capacity; idx++)
+            {
+                output.Add(0.0);
+            }
+
+            for (int I = 0; I < input.Count; I++)
+            {
+                for (int J = 0; J < kernel.Count; J++)
+                {
+                    output[I + J] = output[I + J] + input[I] * kernel[J];
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ TestConvolution/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using DSP;
+
+namespace TestConvolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convolution convolver = new Convolution();
+            List<double> kernel = new List<double>(100);
+            List<double> input = new List<double>(100);
+
+            if (args.Count() < 2)
+            {
+                Console.WriteLine("usage: TestConvolution.exe <kernel file> <input file>");
+                return;
+            }
+
+            FileStream kernelFile = File.OpenRead(args[0]);
+            FileStream inputFile = File.OpenRead(args[1]);
+
+            StreamReader kernelStream = new StreamReader(kernelFile);
+            StreamReader inputStream = new StreamReader(inputFile);
+
+            int kernelIdx = 0;
+            while(kernelStream.Peek() > 0)
+            {
+                string line = kernelStream.ReadLine().Trim();
+
+                //Skip comments
+                if (line[0] == '#')
+                {
+                    continue;
+                }
+
+                kernel.Add(Double.Parse(line));
+                kernelIdx++;
+            }
+
+            int inputIdx = 0;
+            while (inputStream.Peek() > 0)
+            {
+                string line = inputStream.ReadLine().Trim();
+
+                //Skip comments
+                if (line[0] == '#')
+                {
+                    continue;
+                }
+
+                input.Add(Double.Parse(line));
+                inputIdx++;
+            }
+
+            List<double> output = convolver.Convolve(kernel, input);
+
+            Console.WriteLine("# Output signal:");
+
+            foreach (double val in output)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
