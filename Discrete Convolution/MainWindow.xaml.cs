39eb24e580647a2acdec2b9a062ac1c2022552da
Brian Tabone <brian.tabone@gmail.com>
2019-08-11 19:52:36 -0500

    WIP adding load signal and filter CSV files to perform convolution

--- Discrete Convolution/MainWindow.xaml.cs
+++ Discrete Convolution/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Discrete_Convolution.View_Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,41 @@ namespace Discrete_Convolution
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadSignal_Click(object sender, RoutedEventArgs e)
+        {
+            var dataContext = DataContext as ConvolutionViewModel;
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    dataContext.SignalFile = fileDialog.FileName;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
+
+        }
+
+        private void LoadFilterKernel_Click(object sender, RoutedEventArgs e)
+        {
+            var dataContext = DataContext as ConvolutionViewModel;
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    dataContext.FilterFile = fileDialog.FileName;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ Discrete Convolution/View Models/ConvolutionViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace Discrete_Convolution.View_Models
+{
+    // View model for main window, loads signals and filters, applies convolutions, outputs results
+    public class ConvolutionViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private String _errorMessage;
+        public String ErrorMessage {
+            get
+            {
+                return _errorMessage;
+            }
+            private set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        private FileStream _signalFile;
+        public String SignalFile {
+            get
+            {
+                return _signalFile?.Name;
+            }
+
+            set
+            {
+                try
+                {
+                    if (null != _signalFile)
+                    {
+                        _signalFile.Close();
+                    }
+                    _signalFile = new FileStream(value, FileMode.Open);
+                    OnPropertyChanged(nameof(SignalFile));
+                } catch (Exception ex)
+                {
+                    ErrorMessage = ex.Message;
+                }
+            }
+        }
+
+        private FileStream _filterFile;
+        public String FilterFile {
+            get
+            {
+                return _filterFile?.Name;
+            }
+
+            set
+            {
+                try
+                {
+                    if (null != _filterFile)
+                    {
+                        _filterFile.Close();
+                    }
+                    _filterFile = new FileStream(value, FileMode.Open);
+                    OnPropertyChanged(nameof(FilterFile));
+                } catch (Exception ex)
+                {
+                    ErrorMessage = ex.Message;
+                }
+            }
+        }
+    }
+}
