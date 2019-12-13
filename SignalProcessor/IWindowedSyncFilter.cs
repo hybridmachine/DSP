6d08fff33ae4000b331594dbed6957f49334516f
Brian Tabone <brian.tabone@gmail.com>
2019-12-12 22:15:26 -0600

    Work in progress implementing WIndowedSync filters

---
+++ SignalProcessor/IWindowedSyncFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalProcessor
+{
+    public interface IWindowedSyncFilter
+    {
+        double CutoffFrequencySamplingFrequencyPercentage { get; set; }
+        int FilterLength { get; set; }
+
+        List<double> ImpulseResponse();
+    }
+}
---
+++ UnitTests/WindowedSyncFilterTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SignalProcessor;
+using SignalProcessor.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WindowedSyncFilterTest
+    {
+        [TestMethod]
+        public void LowPassWindowedSyncTest()
+        {
+            IWindowedSyncFilter filter = new LowPassWindowedSync();
+        }
+    }
+}
