5071026b00e637f88c124941a45ef01e45999b1f
Brian Tabone <brian.tabone@gmail.com>
2020-02-08 12:00:35 -0600

    Added rudimentary about box

---
+++ Signals And Transforms/View Models/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalsAndTransforms.View_Models
+{
+    public class AboutViewModel
+    {
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string ApplicationInfo
+        {
+            get
+            {
+                return $"{Properties.Resources.APPLICATION_NAME} {Properties.Resources.COPYRIGHT_LABEL} {Properties.Resources.COPYRIGHT} {Properties.Resources.AUTHOR}";
+            }
+        }
+    }
+}
